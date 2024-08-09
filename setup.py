daa47b1534716d250b482fe4880863f549766fae
vivek-metaphy <vivek@metaphy.world>
2024-08-09 22:21:24 +0530

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,24 @@
 # -*- coding: utf-8 -*-
 import os
 import subprocess
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, find_packages
 from setuptools.command.install import install as _install
 
 class InstallCommand(_install):
     def run(self):
+        # Install system dependencies
+        subprocess.check_call([ 'add-apt-repository', 'ppa:jonathonf/ffmpeg-4'])
+        subprocess.check_call([ 'apt-get', 'update'])
+        subprocess.check_call(['apt-get', 'install', '-y', 'build-essential', 'python3-dev', 'python3-setuptools', 'make', 'cmake'])
+        subprocess.check_call([ 'apt-get', 'install', '-y', 'ffmpeg', 'libavcodec-dev', 'libavfilter-dev', 'libavformat-dev', 'libavutil-dev'])
+
         # Build Decord
-        subprocess.check_call(['git', 'clone', '--recursive', 'https://github.com/MetaphyLabs/cuda-decord'])
+        subprocess.check_call(['git', 'clone', '--recursive', 'https://github.com/dmlc/decord'])
+        os.chdir('decord')
         os.makedirs('build', exist_ok=True)
         os.chdir('build')
-        subprocess.check_call(['cmake', '..', '-DUSE_CUDA=ON', '-DCMAKE_BUILD_TYPE=Release'])
+        
+        subprocess.check_call(['cmake', '..', '-DUSE_CUDA=OFF', '-DCMAKE_BUILD_TYPE=Release'])
         subprocess.check_call(['make'])
         
         # Install Decord Python Package
